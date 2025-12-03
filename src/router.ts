5390d63a6d9fca278a5101e490fa5f2e28e3a6ab
emagu <emagudev@gmail.com>
2025-12-03 16:55:33 +0800

    modify

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,17 @@
 // src/router.ts
 import { Router } from 'itty-router'
-
+import liffHtml from './public/index.html'
 // 各 action
 import Login from './employee/login'
 
 const router = Router()
 
 // 掛載 route
-router.post('/api/employee/login', Login)
-
+router.post('/api/employee/login', Login);
+router.get('/', ()=> {
+	return new Response(liffHtml, {
+		headers: { 'Content-Type': 'text/html; charset=utf-8' }
+	});
+});
 // export 出去讓 index.ts 使用
 export { router }
